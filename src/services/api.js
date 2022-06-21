d2c15d868f9b492a9ec3e2eaf410e05658c16ef4
Anna Miklukha <miklukha.anna@ukr.net>
2022-06-21 18:05:20 +0200

    adds axios, creates global styles

---
+++ src/services/api.js
@@ -0,0 +1,2 @@
+// API Key: a9f392cbbc7836cc440c1eab3d6179cd
+// https://api.themoviedb.org/3/movie/550?api_key=a9f392cbbc7836cc440c1eab3d6179cd
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,69 @@
+import { css } from '@emotion/react';
+
+export const GlobalStyles = css`
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    width: 100vw;
+    overflow-x: hidden;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    min-height: 100vh;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    color: #212121;
+    background-color: #ffffff;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  address {
+    font-style: normal;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    font: inherit;
+    color: inherit;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
